4dead96dd7fa7b5581aed6091c493c6fab8b97a6
Toomas Koost <toomas@wizibe.com>
2016-10-10 15:29:36 +0300

    project started

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("WYSIWYG");
+        primaryStage.show();
+
+    }
+}
